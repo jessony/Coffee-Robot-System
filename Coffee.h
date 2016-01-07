2f0e857758f50853d0134d78c9f6c8adb5e28860
jessony <910889414@qq.com>
2016-01-07 22:54:50 +0800

    Update Coffee.h
    现在编译不通过，因为coffee里调用了cup 而现在暂时先定义的coffee类在定义的coffee类，稍后会改过来

--- Coffee.h
+++ Coffee.h
@@ -2,21 +2,83 @@
 #define COFFEE_H
 #include <vector>
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 class Coffee
 {
-	private:
+  private:
   string taste;
   double temperature;
   double price;
+  string volumn;
   public:
-  	void gettaste();
-  	void choosetaste();
-  	void gettemperature();
-  	void choosetemperature();
-  	void getprice();
-  	void showprice();	
+  inline string gettaste(){
+  	return this->taste;
+  }
+  inline	void settaste(string str){
+  	strcpy(this->taste,str);
+  	
+  	
+  	
+  }
+  inline	double gettemperature(){
+  	return this->temperature;	
+  }
+  inline	void settemperature(double tp){
+  	this->temperature=tp;
+  	
+  }
+  inline  void setvolumn(cup cup0)
+  {
+  		strcpy(this->volumne,cup0.size);
+  	
+  }
+  inline	void setprice(){
+  	switch(taste){
+  		case "Machiatto":
+  			switch(volumn){
+  				case "large":
+  				case "middle":	
+  				case"small":
+			  }
+  			
+  		case "Americano":
+  			switch(volumn){
+  				case "large":
+  				case "middle":	
+  				case"small":
+			  }  			
+  		case "Flat white":
+    			switch(volumn){
+  				case "large":
+  				case "middle":	
+  				case"small":
+			  }			
+  		case "Caffè Latte":
+    			switch(volumn){
+  				case "large":
+  				case "middle":	
+  				case"small":
+			  }			
+  		case "Cappuccino":
+  	  			switch(volumn){
+  				case "large":
+  				case "middle":	
+  				case"small":
+			  }		
+  		case "CafeMocha":
+  	  			switch(volumn){
+  				case "large":
+  				case "middle":	
+  				case"small":
+			  }		
+	  }
+  	
+  	
+  }
+  inline	void showprice(){
+  }
+	   	
 	
 };
 class Cup
@@ -24,7 +86,35 @@ class Cup
 	private:
 	string size;
 	int number;
+	public:
+		inline void getsize(){
+			return this->size;	
+		}
+		inline void getnumber(){
+				return this->number;
+		}
+			inline void setsize(){
+			
+		}
+		inline void setnumber(){
+			
+		}
 	
+};
+ class  pay
+ {
+ 	private:
+ 		double totalmoney;
+ 		double givenmoney;
+ 		double charge;
+ 	public:
+ 	  inline void gettotalmoney(){
+ 	  	
+	   }	
+ 			
+ 	
+ };
+#endif
 };
  class  pay
  {
