a709bf53375fe3aa9d32f7ec28cd7a93134369bc
jessony <910889414@qq.com>
2016-01-07 22:07:52 +0800

    addsome detail about coffee by jessony 余拥军

--- Coffee.h
+++ Coffee.h
@@ -1,13 +1,38 @@
 #ifndef COFFEE_H
 #define COFFEE_H
+#include <vector>
+#include <iostream>
+
+using namespace std;
 class Coffee
 {
- 	
+	private:
+  string taste;
+  double temperature;
+  double price;
+  public:
+  	void gettaste();
+  	void choosetaste();
+  	void gettemperature();
+  	void choosetemperature();
+  	void getprice();
+  	void showprice();	
 	
 };
 class Cup
 {
+	private:
+	string size;
+	int number;
 	
 };
-
+ class  pay
+ {
+ 	private:
+ 		double totalmoney;
+ 	public:
+ 	  void gettotalmoney();	
+ 			
+ 	
+ };
 #endif
