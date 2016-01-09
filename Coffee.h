35e81a28e5cebd37a381d399fb88de1b8efdb425
jessony <910889414@qq.com>
2016-01-09 21:05:50 +0800

    Update Coffee.h
    已基本改好，编译通过，该部分可以单独运行

--- Coffee.h
+++ Coffee.h
@@ -3,20 +3,50 @@
 #include <vector>
 #include <iostream>
 #include <cstring>
+#include "math.h"
+#include "Frame.h"
+#include"stdio.h"
 using namespace std;
+
+class Cup
+{
+	private:
+	string size;
+	int number;
+	public:
+		inline string getsize(){
+			return this->size;	
+		}
+		inline int getnumber(){
+				return this->number;
+		}
+			inline void setsize(){
+			
+		}
+		inline void setnumber(){
+			
+		}
+	
+};
+
 class Coffee
 {
   private:
   string taste;
   double temperature;
-  double price;
+  double price;//(初始化应置0)
   string volumn;
+  Point position;
+  Cup cup0;
   public:
+  	inline  void setposition(Point &p){
+  	 this->position.setpoint(p.getx(),p.gety());
+	  }
   inline string gettaste(){
   	return this->taste;
   }
   inline	void settaste(string str){
-  	strcpy(this->taste,str);
+  	this->taste.assign(str);
   	
   	
   	
@@ -28,101 +58,67 @@ class Coffee
   	this->temperature=tp;
   	
   }
-  inline  void setvolumn(cup cup0)
+  inline  void setvolumn(Cup &cup0)
   {
-  		strcpy(this->volumne,cup0.size);
+  		this->volumn.assign(cup0.getsize());
   	
   }
   inline	void setprice(){
-  	switch(taste){
-  		case "Machiatto":
-  			switch(volumn){
-  				case "large":
-  				case "middle":	
-  				case"small":
-			  }
-  			
-  		case "Americano":
-  			switch(volumn){
-  				case "large":
-  				case "middle":	
-  				case"small":
-			  }  			
-  		case "Flat white":
-    			switch(volumn){
-  				case "large":
-  				case "middle":	
-  				case"small":
-			  }			
-  		case "Caffè Latte":
-    			switch(volumn){
-  				case "large":
-  				case "middle":	
-  				case"small":
-			  }			
-  		case "Cappuccino":
-  	  			switch(volumn){
-  				case "large":
-  				case "middle":	
-  				case"small":
-			  }		
-  		case "CafeMocha":
-  	  			switch(volumn){
-  				case "large":
-  				case "middle":	
-  				case"small":
-			  }		
-	  }
+  	if (this->taste=="Machiatto"&&this->cup0.getsize()=="large") this->price=10;
+  	if (this->taste=="Machiatto"&&this->cup0.getsize()=="middle") this->price=8;
   	
+  	if (this->taste=="Machiatto"&&this->cup0.getsize()=="small") this->price=6;
+  	if (this->taste=="Americano"&&this->cup0.getsize()=="large") this->price=9;
+  		if (this->taste=="Americano"&&this->cup0.getsize()=="middle") this->price=7;
+  			if (this->taste=="Americano"&&this->cup0.getsize()=="small") this->price=5;
+  			if (this->taste=="Flat white"&&this->cup0.getsize()=="large") this->price=11;
+  			if (this->taste=="Flat white"&&this->cup0.getsize()=="middle") this->price=9;
+  			if (this->taste=="Flat white"&&this->cup0.getsize()=="small") this->price=7;
+			if (this->taste=="Caffè Latte"&&this->cup0.getsize()=="large") this->price=10;	
+			if (this->taste=="Caffè Latte"&&this->cup0.getsize()=="middle") this->price=7;
+			if (this->taste=="Caffè Latte"&&this->cup0.getsize()=="small") this->price=5;
+			if (this->taste=="Cappuccino"&&this->cup0.getsize()=="large") this->price=12;
+			if (this->taste=="Cappuccino"&&this->cup0.getsize()=="middle") this->price=10;
+			if (this->taste=="Cappuccino"&&this->cup0.getsize()=="small") this->price=8;
+			if (this->taste=="CafeMocha"&&this->cup0.getsize()=="large") this->price=10;
+			if (this->taste=="CafeMocha"&&this->cup0.getsize()=="middle") this->price=8;
+			if (this->taste=="CafeMocha"&&this->cup0.getsize()=="small") this->price=6;
   	
   }
   inline	void showprice(){
+  	//鼠标点击不同咖啡时，显示对应不同价格 
   }
-	   	
+  
+ inline	double getprice(){
+ 	
+  return this->price;
+  }
+  	   	
 	
 };
-class Cup
-{
-	private:
-	string size;
-	int number;
-	public:
-		inline void getsize(){
-			return this->size;	
-		}
-		inline void getnumber(){
-				return this->number;
-		}
-			inline void setsize(){
-			
-		}
-		inline void setnumber(){
-			
-		}
-	
-};
+
  class  pay
  {
  	private:
- 		double totalmoney;
+ 		double totalmoney;//(初始化应置0)
  		double givenmoney;
  		double charge;
+ 		Coffee coffee0;
  	public:
- 	  inline void gettotalmoney(){
- 	  	
+ 	  inline double gettotalmoney(){
+ 	  	totalmoney+=coffee0.getprice();
+ 	  	return this->totalmoney;
 	   }	
- 			
- 	
- };
-#endif
-};
- class  pay
- {
- 	private:
- 		double totalmoney;
- 	public:
- 	  void gettotalmoney();	
- 			
+	  inline double getcharge(){
+	  	this->charge=this->givenmoney-this->gettotalmoney();
+	   	return this->charge;
+	   	
+	   }
+	     inline double setgivenmoney(double gm){
+	     	this->givenmoney=gm;
+	   	return this->givenmoney;
+	   	
+	   }	
  	
  };
 #endif
